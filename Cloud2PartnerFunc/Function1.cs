b8e6dc59a98c01605071703141fe2ab252ee1a14
Allen Smith <AllenPSmith@hotmail.com>
2018-01-11 23:08:51 +0000

    Initial Commint for the Advanced Cloud 2 Partner Data Function

---
+++ Cloud2PartnerFunc/Function1.cs
@@ -0,0 +1,57 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace Cloud2PartnerFunc
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([ServiceBusTrigger("cloud2fidotopic", "Partner", AccessRights.Manage, Connection = "Connection")]string mySbMsg, TraceWriter log)
+        {
+            log.Info($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "cloud2fidosqlsvr.database.windows.net";
+                builder.UserID = "cloud2fido";
+                builder.Password = "Starbucks1234!";
+                builder.InitialCatalog = "cloud2fidosqldb2";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    log.Info($"\nQuery data example:");
+                    log.Info($"=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT TOP 20 pc.PERSONNUM as PersonNumber, pc.PERSONFULLNAME as PersonFullName ");
+                    sb.Append("FROM [Partner] pc ");
+                    string sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                log.Info($"{reader.GetString(0)} {reader.GetString(1)}");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                log.Info($"{e.ToString()}");
+            }
+            
+        }
+
+         
+    }
+}
